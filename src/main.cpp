f7ce197ba7e146fc4e747004143f022e89dcb7a9
MUSTAFA EGE CIKLABAKKAL <ege.cikla@gmail.com>
2019-02-14 20:08:45 +0300

    Simple Raytracer added, bugs exist

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,49 @@
-#include "rgb.h"
-#include "image.h"
-#include "vector3.h"
-#include "ray.h"
-#include "triangle.h"
-#include <fstream>
+#include "utils.h"
 
 int main(int argc, char* argv[])
 {
-	Ray r;
+	int width, height;
+	hitRecord record;
+	bool hit;
+	float tmax;
+	Vec3 direction(0, 0, -1);
 
-	std::cout << r << std::endl;
+	width = 500; height = 500;
+	Sphere sphere = Sphere(Vec3(250, 250, -1000), 150, rgb(0.2f, 0.1f, 0.8f));
+	Triangle triangle = Triangle(Vec3(300, 600, -800), Vec3(0, 100, -1000), Vec3(450, 20, -1000), rgb(0.9f, 0.2f, 0.1f));
+	std::vector<Shape*> shapes = {&sphere, &triangle};
+	Image img(width, height);
+
+	tmax = 100000.0f;
+	for(int i = 0; i < width; i++)
+	{
+		for(int j = 0; j < height; j++)
+		{
+			hit = false;
+			Ray r(Vec3(i, j, 0), direction);
+
+			for(const Shape* shape : shapes)
+			{
+				if(shape->hit(r, 0.0001f, tmax, 0.0f, record))
+				{
+					tmax = record.t;
+					hit = true;
+				}
+			}
+
+			if(hit)
+			{
+				img.set(i, j, record.color);
+			}
+	
+			else
+			{
+				img.set(i, j, rgb(0.1f, 0.1f, 0.1f));
+			}
+		}
+	}
+
+	img.writePNG("simple.png");
 
 	return 0;
 }
---
+++ src/utils.h
@@ -0,0 +1,13 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include <vector>
+#include <iostream>
+#include "vector3.h"
+#include "rgb.h"
+#include "image.h"
+#include "shape.h"
+#include "triangle.h"
+#include "sphere.h"
+
+#endif
