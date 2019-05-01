4c6359623791e886c7205b9d308116d52d4bc1e5
EgeCiklabakkal <ege.cikla@gmail.com>
2019-05-01 11:43:25 +0300

    tonemapping fix

--- src/image.cpp
+++ src/image.cpp
@@ -242,7 +242,8 @@ bool Image::handleTonemap(const Tonemap& tonemap, std::vector<unsigned char>& da
 		for(int i = 0; i < N; i++)
 		{
 			Ld = (Ls[i] * (1.0f + (Ls[i] / Lwhite))) / (1.0f + Ls[i]);
-			Ls[i] = std::clamp(Ld, 0.0f, 1.0f);
+			//Ls[i] = std::clamp(Ld, 0.0f, 1.0f);
+			Ls[i] = Ld;
 		}
 
 		// calculate display values in rgb
